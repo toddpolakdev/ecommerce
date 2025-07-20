189ea4538f7440978882052a5af123ac8c7facd4
Todd Polak <toddpolakdev@gmail.com>
2025-07-19 19:07:53 -0500

    Added support for CORS

--- src/main/java/com/myproject/ecommerce/dao/ProductCategoryRepository.java
+++ src/main/java/com/myproject/ecommerce/dao/ProductCategoryRepository.java
@@ -4,7 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.myproject.ecommerce.entity.ProductCategory;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200")
 @RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
 
--- src/main/java/com/myproject/ecommerce/dao/ProductRepository.java
+++ src/main/java/com/myproject/ecommerce/dao/ProductRepository.java
@@ -2,8 +2,10 @@ package com.myproject.ecommerce.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.myproject.ecommerce.entity.Product;;
+import com.myproject.ecommerce.entity.Product;
+import org.springframework.web.bind.annotation.CrossOrigin;;
 
+@CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
 }
