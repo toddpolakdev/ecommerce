dd5fcadc67f67a38c414a10634b23b5f8c25a884
Todd Polak <toddpolakdev@gmail.com>
2025-07-21 13:14:19 -0500

    routing

--- src/main/java/com/myproject/ecommerce/dao/ProductRepository.java
+++ src/main/java/com/myproject/ecommerce/dao/ProductRepository.java
@@ -1,11 +1,14 @@
 package com.myproject.ecommerce.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.myproject.ecommerce.entity.Product;
-import org.springframework.web.bind.annotation.CrossOrigin;;
+import org.springframework.data.repository.query.Param;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 @CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
+    Page<Product> findByCategoryId(@Param("id") Long id, Pageable pageable);
 }
